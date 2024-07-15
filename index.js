089eea5baf5f5cd200bd8dcca177613d365ac347
Ndoria <k.ndoriamurigu@gmail.com>
2024-07-16 01:28:05 +0300

    complete labs

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+    const input = document.getElementById('button')
+
+    function clickAlert() {
+    alert('I was clicked!')
 }
+  //.addEventListener('click',function() {
+  //  console.log('I was clicked!');
+    input.addEventListener('click', clickAlert)
+
+}
+
+addingEventListener();
